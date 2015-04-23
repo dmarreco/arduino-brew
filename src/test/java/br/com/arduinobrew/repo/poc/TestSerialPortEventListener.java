bb8409e0cd3dcea663b34221078c782e1521d693
Daniel Marreco <daniel.marreco@gmail.com>
2015-04-23 19:23:59 -0300

    commits antigos que estavam na minha maquina

---
+++ src/test/java/br/com/arduinobrew/repo/poc/TestSerialPortEventListener.java
@@ -0,0 +1,11 @@
+package br.com.arduinobrew.repo.poc;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dmarreco
+ * Date: 6/19/14
+ * Time: 5:59 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestSerialPortEventListener {
+}
